d519484e0f1d1be69d91194fbc3b182718cb0005
AndreasKuncoro11788 <220711788@students.uajy.ac.id>
2024-10-08 23:49:58 +0700

    Edit Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:2_B_Gym/View/home.dart';
-import 'package:2_B_Gym/View/login.dart';
-import 'package:2_B_Gym/View/profile.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeView(),
-    );
-  }
-}
-=======
-import 'package:flutter/material.dart';
 import 'package:flutter_application_1/view/login.dart';
 
 void main() {
