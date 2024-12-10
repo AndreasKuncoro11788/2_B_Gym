996d10eca76d7d50ac48750f0b092f6ce97ec273
NikolausAdhiKristianto <220711798@students.uajy.ac.id>
2024-12-10 12:29:46 +0700

    FIx
    Fix

--- lib/client/RiwayatPemesananClient.dart
+++ lib/client/RiwayatPemesananClient.dart
@@ -12,14 +12,17 @@ class RiwayatPemesananClient {
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body)['data'] ?? [];
-      return jsonResponse.map((data) => RiwayatPemesanan.fromJson(data)).toList();
+      return jsonResponse
+          .map((data) => RiwayatPemesanan.fromJson(data))
+          .toList();
     } else {
       throw Exception('Failed to load riwayat pemesanan');
     }
   }
 
   // Fungsi untuk membuat riwayat pemesanan baru
-  static Future<RiwayatPemesanan> createRiwayatPemesanan(RiwayatPemesanan riwayatPemesanan) async {
+  static Future<RiwayatPemesanan> createRiwayatPemesanan(
+      RiwayatPemesanan riwayatPemesanan) async {
     final response = await http.post(
       Uri.parse('$url$endpoint'),
       headers: <String, String>{
@@ -36,7 +39,8 @@ class RiwayatPemesananClient {
   }
 
   // Fungsi untuk mengupdate riwayat pemesanan (termasuk review dan rating)
-  static Future<RiwayatPemesanan> updateRiwayatPemesanan(RiwayatPemesanan riwayatPemesanan) async {
+  static Future<RiwayatPemesanan> updateRiwayatPemesanan(
+      RiwayatPemesanan riwayatPemesanan) async {
     try {
       final response = await http.put(
         Uri.parse('$url$endpoint/${riwayatPemesanan.id}'),
@@ -44,8 +48,8 @@ class RiwayatPemesananClient {
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: json.encode({
-          'rating': riwayatPemesanan.rating,  // Send rating
-          'review': riwayatPemesanan.review,  // Send review
+          'rating': riwayatPemesanan.rating, // Send rating
+          'review': riwayatPemesanan.review, // Send review
         }),
       );
 
@@ -54,7 +58,8 @@ class RiwayatPemesananClient {
         return RiwayatPemesanan.fromJson(json.decode(response.body)['data']);
       } else {
         // Handle non-200 responses
-        throw Exception('Failed to update riwayat pemesanan: ${response.statusCode}');
+        throw Exception(
+            'Failed to update riwayat pemesanan: ${response.statusCode}');
       }
     } catch (e) {
       // Handle any errors that occur during the HTTP request
@@ -83,17 +88,21 @@ class RiwayatPemesananClient {
     }
   }
 
-  static Future<List<RiwayatPemesanan>> fetchRiwayatPemesananByPembayaranId(int idPembayaran) async {
-  final response = await http.get(Uri.parse('$url$endpoint')); // Ambil semua data riwayat
+  static Future<List<RiwayatPemesanan>> fetchRiwayatPemesananByPembayaranId(
+      int idPembayaran) async {
+    final response =
+        await http.get(Uri.parse('$url$endpoint')); // Ambil semua data riwayat
 
-  if (response.statusCode == 200) {
-    List jsonResponse = json.decode(response.body)['data'];
-    List<RiwayatPemesanan> allRiwayat = jsonResponse.map((data) => RiwayatPemesanan.fromJson(data)).toList();
-    List<RiwayatPemesanan> filteredRiwayat = allRiwayat.where((riwayat) => riwayat.id_pembayaran == idPembayaran).toList();
-    return filteredRiwayat;
-  } else {
-    throw Exception('Gagal mengambil data riwayat pemesanan');
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body)['data'];
+      List<RiwayatPemesanan> allRiwayat =
+          jsonResponse.map((data) => RiwayatPemesanan.fromJson(data)).toList();
+      List<RiwayatPemesanan> filteredRiwayat = allRiwayat
+          .where((riwayat) => riwayat.id_pembayaran == idPembayaran)
+          .toList();
+      return filteredRiwayat;
+    } else {
+      throw Exception('Gagal mengambil data riwayat pemesanan');
+    }
   }
-}
-
 }
--- lib/entity/Pembayaran.dart
+++ lib/entity/Pembayaran.dart
@@ -1,22 +1,23 @@
 import 'dart:convert';
 
 class Pembayaran {
-  final int id;
+  final int? id;
   final int id_pemesanan;
   final String jenisPembayaran;
   final String statusPembayaran;
   final int totalPembayaran;
 
   Pembayaran({
-    required this.id,
+    this.id,
     required this.id_pemesanan,
     required this.jenisPembayaran,
     required this.statusPembayaran,
     required this.totalPembayaran,
-
   });
 
-  factory Pembayaran.fromRawJson(String str) => Pembayaran.fromJson(json.decode(str));
+  factory Pembayaran.fromRawJson(String str) =>
+      Pembayaran.fromJson(json.decode(str));
+
   factory Pembayaran.fromJson(Map<String, dynamic> json) {
     return Pembayaran(
       id: json["id"],
@@ -28,11 +29,12 @@ class Pembayaran {
   }
 
   String toRawJson() => json.encode(toJson());
+
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "id_pemesanan": id_pemesanan,
-    "jenisPembayaran": jenisPembayaran,
-    "statusPembayaran": statusPembayaran,
-    "totalPembayaran": totalPembayaran,
-  };
+        "id": id,
+        "id_pemesanan": id_pemesanan,
+        "jenisPembayaran": jenisPembayaran,
+        "statusPembayaran": statusPembayaran,
+        "totalPembayaran": totalPembayaran,
+      };
 }
